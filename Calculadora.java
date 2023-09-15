18a12c1d858fb6b058b20ea6ef93a0b2ae37a382
MEL PLENS ANGELIS <99374140+MelPLens@users.noreply.github.com>
2023-09-15 16:51:51 -0300

    Update Calculadora.java

--- Calculadora.java
+++ Calculadora.java
@@ -1,25 +1,26 @@
 public class Calculadora {
 	
 	public class Soma {
-	    public static int somar(int a, int b) {
-	        return a + b; // retorna o valor esperado
+	public static int somar(int a, int b) {  //definida como estática,podem ser chamados diretamente a partir da
+       //classe Calculadora, sem a necessidade de criar uma instância da Calculadora.
+	return a + b; // Retorna a soma de a e b
 	    }
 	    
-	    public class Subtracao{
-		 public static int somar(int a, int b) {
-		        return a - b;
+	public class Subtracao{
+	public static int somar(int a, int b) { // Método renomeado para subtrair
+	return a - b; // Retorna a subtracao de a e b
 		    }
 	
 	    
-		 public class Divisao{
-		  public static int somar(int a, int b) {
-			        return a / b;
+	public class Divisao{
+	public static int somar(int a, int b) {
+	return a / b; // Retorna a divisao de a e b 
 			    }
 		  
 		  
-		  public class multiplicacao{
-			  public static int somar(int a, int b) {
-				        return a * b;
+	public class multiplicacao{
+        public static int somar(int a, int b) {
+	return a * b; // Retorna a divisao de a e b
 			 }
 	    
 	  
